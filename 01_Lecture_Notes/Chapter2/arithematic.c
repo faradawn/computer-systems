a0d357c608d683a616da54d3f94aeccaf7e73617
faradawn <faradawny@gmail.com>
2021-07-01 18:19:56 +0800

    2.1 arithematics

---
+++ 01_Lecture_Notes/Chapter2/arithematic.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+// encoding integer
+#include <stdio.h>
+
+int main(){
+  printf("hello\n");
+  printf("%u\n", 0b1011);
+  printf("%d\n", 0b1011);
+  int a = 0b1011;
+  printf("%d\n", a);
+
+
+}
