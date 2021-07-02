f829b4a0186a2c15345b0dff6f8221adf37a8a8f
faradawn <faradawny@gmail.com>
2021-07-02 12:01:08 +0800

    unsigned arithmetic

--- 01_Lecture_Notes/Chapter2/arithematic.c
+++ 01_Lecture_Notes/Chapter2/arithematic.c
@@ -4,11 +4,8 @@
 #include <stdio.h>
 
 int main(){
-  printf("hello\n");
-  printf("%u\n", 0b1011);
-  printf("%d\n", 0b1011);
-  int a = 0b1011;
-  printf("%d\n", a);
+  printf("%d\n", 1-(unsigned)2);
+
 
 
 }
